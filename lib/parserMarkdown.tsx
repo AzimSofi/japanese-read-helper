3f4053f1aa07d54193cd0557d80089849f449a9c
Azim Sofi <azimmiskandar@gmail.com>
2025-08-11 18:17:28 +0900

    Markdownパーサーを強化、デバッグログを追加
    Markdownパーサーを改善し、見出しやサブアイテム（特に予期せぬプレフィックスがないケース）の処理能力を向上。ビジュアルノベルページには、API応答フォーマットのトラブルシューティングを支援するため、より詳細なデバッグログを追加した。

--- lib/parserMarkdown.tsx
+++ lib/parserMarkdown.tsx
@@ -8,36 +8,51 @@ export function parseMarkdown(text: string): ParsedItem[] {
   const headingPrefix = "<";
   const subItemPrefix = ">>";
   const parsedData: ParsedItem[] = [];
-  let previousLine = "";
   let currentHeadItem: ParsedItem | null = null;
 
-  splitByLines.forEach((line) => {
+  for (let i = 0; i < splitByLines.length; i++) {
+    const line = splitByLines[i];
+    const previousLineContent = i > 0 ? splitByLines[i - 1] : "";
+
     if (line.startsWith(headingPrefix)) {
       currentHeadItem = { head: line.slice(1), subItems: [] };
       parsedData.push(currentHeadItem);
-      previousLine = line;
+    } else if (
+      // 元々Headですが、〝headingPrefix〟はない、
+      // Head = {}
+      i + 3 < splitByLines.length &&
+      splitByLines[i + 1].startsWith(subItemPrefix) &&
+      splitByLines[i + 2].startsWith(subItemPrefix) &&
+      splitByLines[i + 3].startsWith(subItemPrefix)
+    ) {
+      currentHeadItem = { head: line, subItems: [] };
+      parsedData.push(currentHeadItem);
     } else if (line.startsWith(subItemPrefix)) {
       const trimmedSubItem = line.slice(2);
       if (currentHeadItem) {
         currentHeadItem.subItems.push(trimmedSubItem);
-        previousLine = line;
       }
     } else {
       if (line.trim().length > 0 && currentHeadItem) {
-        if (previousLine.startsWith(headingPrefix)) {
+        // Head + {}
+        if (previousLineContent.startsWith(headingPrefix)) {
           const unfinishedHeadItem = { head: line, subItems: [] };
           currentHeadItem.head += "、" + unfinishedHeadItem.head;
-        } else if (previousLine.startsWith(subItemPrefix)) {
-          for (let i = 0; i < currentHeadItem.subItems.length; i++) {
-            if (currentHeadItem.subItems[i] == previousLine.slice(2)) {
-              currentHeadItem.subItems[i] += "、" + line;
+        }
+        // item + {}
+        else if (previousLineContent.startsWith(subItemPrefix)) {
+          for (let j = 0; j < currentHeadItem.subItems.length; j++) {
+            if (currentHeadItem.subItems[j] == previousLineContent.slice(2)) {
+              currentHeadItem.subItems[j] += "、" + line;
             }
           }
-        } else {
+        }
+        // 変な形
+        else {
           console.log("Unexpected line format:", line);
         }
       }
     }
-  });
+  }
   return parsedData;
 }
