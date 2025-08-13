22083f21010b0b75d2303b2b28af7d0d7ea4148b
Azim Sofi <azimmiskandar@gmail.com>
2025-08-13 22:33:48 +0900

    頭が壊れそう
    `>>後ろから美咲の控えめな笑い声が聞こえた。<「いいのか？」`この文字列が出る際の整理機能を追加しました

--- lib/parserMarkdown.tsx
+++ lib/parserMarkdown.tsx
@@ -6,6 +6,7 @@ interface ParsedItem {
 export function parseMarkdown(text: string): ParsedItem[] {
   const splitByLines = text.split("\n");
   const headingPrefix = "<";
+  const headingPrefixV2 = "＜";
   const subItemPrefix = ">>";
   const parsedData: ParsedItem[] = [];
   let currentHeadItem: ParsedItem | null = null;
@@ -14,9 +15,47 @@ export function parseMarkdown(text: string): ParsedItem[] {
     const line = splitByLines[i];
     const previousLineContent = i > 0 ? splitByLines[i - 1] : "";
 
-    if (line.startsWith(headingPrefix)) {
+    if (line.startsWith(headingPrefix) || line.startsWith(headingPrefixV2)) {
       currentHeadItem = { head: line.slice(1), subItems: [] };
       parsedData.push(currentHeadItem);
+    } else if (
+      i + 5 < splitByLines.length &&
+      splitByLines[i + 1].startsWith(subItemPrefix) &&
+      splitByLines[i + 2].startsWith(subItemPrefix) &&
+      splitByLines[i + 3].startsWith(subItemPrefix) &&
+      splitByLines[i + 4].startsWith(subItemPrefix)
+    ) {
+      // Headprefixは文書の中に入ってしまう
+      // const oddLine = splitByLines[i + 2];
+      // if (oddLine.includes(headingPrefix)) {
+      //   console.log(splitByLines[i+2]);
+      // } else if (oddLine.includes(headingPrefixV2)) {
+      //   console.log(splitByLines[i+2]);
+      // } else {
+      //   console.log("Unexpected line format:", line);
+      // }
+      
+      const trimmedSubItem = line.slice(2);
+      if (currentHeadItem) {
+        currentHeadItem.subItems.push(trimmedSubItem);
+        currentHeadItem.subItems.push(splitByLines[i+1].slice(2));
+      }
+
+      if (splitByLines[i+2].includes(headingPrefix)) {
+        const oddLine = splitByLines[i+2].split(headingPrefix);
+        const part1 = oddLine[0];
+        const part2 = oddLine[1];
+        splitByLines[i+1] = part1;
+        splitByLines[i+2] = part2;
+      } else if (splitByLines[i+2].includes(headingPrefixV2)) {
+        const oddLine = splitByLines[i+2].split(headingPrefixV2);
+        const part1 = oddLine[0];
+        const part2 = oddLine[1];
+        splitByLines[i+1] = part1;
+        splitByLines[i+2] = part2;
+      } else {
+        console.log("Unexpected line format:", splitByLines[i+2]);
+      }
     } else if (
       // 元々Headですが、〝headingPrefix〟はない、
       // Head = {}
