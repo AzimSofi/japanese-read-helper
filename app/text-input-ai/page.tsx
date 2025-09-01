fe8cc6fb2eb6d2046f929be699a4b0a537f704f2
AQ-AzimSofi <AQ-AzimSofi@users.noreply.github.com>
2025-09-01 13:23:25 +0900

    エラー克服できた！　
    嬉しい満々、、
    
    って言うんですかｗ

--- app/text-input-ai/page.tsx
+++ app/text-input-ai/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         e.preventDefault();
         if (isLoading) return;
         setIsLoading(true);
-        const response = await fetch("/api/save-text-ai", {
+        const response = await fetch("/api/write-public-txt-ai", {
             method: "POST",
             headers: { "Content-Type": "application/json", },
             body: JSON.stringify({ text: inputText }),
