858958548715b22d3c36e56547c8cabdff407800
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2025-09-20 09:29:10 +0900

    sidebar, flexcolに更新

--- app/layout/sidebar.tsx
+++ app/layout/sidebar.tsx
@@ -5,17 +5,34 @@ interface SidebarProps {
   dropdownAlwaysOpenState: boolean;
 }
 
-export default function Sidebar({setDropdownAlwaysOpenState, dropdownAlwaysOpenState}: SidebarProps) {
-
-  return (
-    <div className="fixed bottom-2 left-0 h-full z-50 text-xs">
-        <button
-            onClick={() => setDropdownAlwaysOpenState(!dropdownAlwaysOpenState)}
-            className={`hover:underline outline-1 m-5 p-2 cursor-pointer
+export default function Sidebar({
+  setDropdownAlwaysOpenState,
+  dropdownAlwaysOpenState,
+}: SidebarProps) {
+  return ( 
+    <div className="fixed bottom-2 left-0 h-full z-50 text-xs flex flex-col items-center">
+      <button
+        onClick={() => setDropdownAlwaysOpenState(!dropdownAlwaysOpenState)}
+        className={`hover:underline outline-1 m-5 p-2 cursor-pointer
                     ${dropdownAlwaysOpenState ? "bg-white" : "bg-grey-50"}`}
-        >
-            {dropdownAlwaysOpenState ? "表示" : "隠す"}
-        </button>   
+      >
+        {dropdownAlwaysOpenState ? "表示" : "隠す"}
+      </button>
+      <a
+        href="#"
+        className="hover:underline"
+        onClick={e => {
+          e.preventDefault();
+          const el = document.getElementById("bookmark");
+          if (el) {
+            const offset = -window.innerHeight / 5 + el.offsetHeight / 5;
+            const top = el.getBoundingClientRect().top + window.scrollY + offset;
+            window.scrollTo({ top, behavior: "smooth" });
+          }
+        }}
+      >
+        ブックマークへ
+      </a>
     </div>
-  )
+  );
 }
